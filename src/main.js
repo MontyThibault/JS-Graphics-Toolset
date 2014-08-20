cb62df852b8ce4686d77be22f4e97a19e250e30a
Monty Thibault <montythibault@gmail.com>
2014-08-19 19:31:56 -0600

    Updated libs

--- src/main.js
+++ src/main.js
@@ -9,36 +9,38 @@
 		engine.loader.fadeOut();
 	});
 
+	console.log('Hello, world!');
 
-	var sampleMap = new engine.grid.BooleanGrid({
-		x: 128,
-		y: 128
-	});
+
+	// var sampleMap = new engine.grid.BooleanGrid({
+	// 	x: 128,
+	// 	y: 128
+	// });
 	
-	var scene = new THREE.Scene();
+	// var scene = new THREE.Scene();
 
-    var grid = new engine.overlays.Color(new THREE.Box2(new THREE.Vector2(), new THREE.Vector2(128, 128)));
+ //    var grid = new engine.overlays.Color(new THREE.Box2(new THREE.Vector2(), new THREE.Vector2(128, 128)));
 
-	function newColors() {
-		var v = grid.colorData.view;
+	// function newColors() {
+	// 	var v = grid.colorData.view;
 
-		var square = Math.floor(Math.random() * 128 * 128 * 3);
-		v[square] = Math.random() * 255;
-		v[square + 1] = Math.random() * 255;
-		v[square + 2] = Math.random() * 255;
+	// 	var square = Math.floor(Math.random() * 128 * 128 * 3);
+	// 	v[square] = Math.random() * 255;
+	// 	v[square + 1] = Math.random() * 255;
+	// 	v[square + 2] = Math.random() * 255;
 
-		grid.texture.needsUpdate = true;
-	}
+	// 	grid.texture.needsUpdate = true;
+	// }
 
-	(function frame() {
-		engine.camera.update();
-		newColors();
-		engine.display.render(scene, engine.camera.cam);
+	// (function frame() {
+	// 	engine.camera.update();
+	// 	newColors();
+	// 	engine.display.render(scene, engine.camera.cam);
 
-		if(engine.fps === 60) {
-            window.requestAnimationFrame(frame);
-		} else {
-            window.setTimeout(frame, 1000 / engine.fps);
-		}
-	})();
+	// 	if(engine.fps === 60) {
+ //            window.requestAnimationFrame(frame);
+	// 	} else {
+ //            window.setTimeout(frame, 1000 / engine.fps);
+	// 	}
+	// })();
 })(engine);
